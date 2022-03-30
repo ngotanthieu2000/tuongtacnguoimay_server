9b9c51241fc8fcd3681f31b0a36b3b6142be7bc7
ngotanthieu2000 <thieucoder@gmail.com>
2022-03-30 13:50:38 +0700

    Add api create and delete for family models

--- routers/FamilysRouter.js
+++ routers/FamilysRouter.js
@@ -8,4 +8,15 @@ router.get('/', async (req,res)=>{
     res.status(404).json(getFamily)
 })
 
+router.post('/create',async (req,res)=>{
+    const createFamily = new FamilysModel(req.body)
+    if(!createFamily) res.status(403).json({Message:"Error, please try again"})
+    res.status(200).json(createFamily)
+})
+
+router.delete('/delete', async (req,res) =>{
+    const deleteFamily = await FamilysModel.findByIdAndDelete({_id:req.body.id})
+    if(!deleteFamily) res.status(403).json({Message:"Error, please try again"})
+    res.status(200).json(deleteFamily)
+})
 module.exports = router;
