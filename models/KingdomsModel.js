e098a89b2a562ad8e54527523aabeeea11bc62d6
ngotanthieu2000 <thieucoder@gmail.com>
2022-03-30 15:48:59 +0700

    remove KingdomsModels.js

--- models/KingdomsModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose;
-
-const KingdomsSchema = new Schema({
-    kingdomsName:{type:String, required:true, unique:true},
-},{timestamps:true})
-module.exports = mongoose.model('Kingdoms',KingdomsSchema)
