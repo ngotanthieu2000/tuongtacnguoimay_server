162ef2b01a3e4f36ec2af3c43fa3b68944dd1eb0
trantantrung <trantantrung@gmail.com>
2022-04-24 16:58:14 +0700

    delete file crash

--- models/CoordinatesModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose;
-
-const CoordinateSchema = new Schema({
-    lat:{type:String , required:true},
-    long:{type:String, required:true},
-},{timestamps:true})
-module.exports = mongoose.model('Coordinates',CoordinateSchema)
